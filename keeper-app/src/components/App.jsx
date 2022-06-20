c1079de875bad724ad4e8bb8f49b9d7382155073
Hanmai001 <88010632+Hanmai001@users.noreply.github.com>
2022-06-20 15:13:22 +0700

    Create notes

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Heading from "./Heading";
 import Footer from "./Footer"
+import Content from "./Content";
+import notes from "../notes.js";
 import Note from "./Note";
-import Content from "./Content"
 
 function App() {
-    return <div><Heading /><Note /><Content /><Footer /></div>
+    return (
+        <div>
+            <Heading />
+            {notes.map((note) => (<Note key={note.key} title={note.title} content={note.content} />))}
+            <Content />
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
