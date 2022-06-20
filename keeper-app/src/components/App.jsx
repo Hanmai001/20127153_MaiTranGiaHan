974bcac54ccdecd49ce606c2642409dcaede68dd
Hanmai001 <88010632+Hanmai001@users.noreply.github.com>
2022-06-20 15:45:17 +0700

    Add  heading icon

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Heading from "./Heading";
 import Footer from "./Footer"
-import Content from "./Content";
 import notes from "../notes.js";
 import Note from "./Note";
 
@@ -10,7 +9,6 @@ function App() {
         <div>
             <Heading />
             {notes.map((note) => (<Note key={note.key} title={note.title} content={note.content} />))}
-            <Content />
             <Footer />
         </div>
     );
--- keeper-app/src/components/Heading.jsx
+++ keeper-app/src/components/Heading.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Heading() {
-    return <header><h1>KEEPER APP</h1></header>
+    return <header><h1><i className="header-icon ti-notepad"></i>KEEPER APP</h1></header>
 };
 
 export default Heading;
